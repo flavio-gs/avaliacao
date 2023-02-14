71a643098dc85c5e952a55c345eb9300b7dbc298
flavio-gs <104517831+flavio-gs@users.noreply.github.com>
2023-02-14 11:57:47 -0300

    Add files via upload

---
+++ ipep/src/main/java/com/igreja/ipep/IpepApplication.java
@@ -0,0 +1,13 @@
+package com.igreja.ipep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpepApplication.class, args);
+	}
+
+}
